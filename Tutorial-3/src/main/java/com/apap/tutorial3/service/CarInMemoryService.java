25bbd5ea4cfe3b4152898fd9a116c2713a5438f1
zhelia alifa <zheliaalifa@gmail.com>
2018-09-25 18:45:50 +0700

    Initial Tutorial 3

---
+++ Tutorial-3/src/main/java/com/apap/tutorial3/service/CarInMemoryService.java
@@ -0,0 +1,49 @@
+package com.apap.tutorial3.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.apap.tutorial3.model.CarModel;
+
+
+import org.springframework.stereotype.Service;
+@Service
+public class CarInMemoryService implements CarService{
+	private List<CarModel> archiveCar;
+	
+	public CarInMemoryService() {
+		archiveCar = new ArrayList<>();
+	}
+	
+	@Override
+	public void addCar(CarModel car) {
+		archiveCar.add(car);
+	}
+	
+	@Override
+	public List<CarModel> getCarList() {
+		return archiveCar;
+	}
+
+	@Override
+	public CarModel getCarDetail(String id) {
+		// TODO Auto-generated method stub
+		for (int i=0; i<archiveCar.size(); i++) {
+			if(archiveCar.get(i).getId().equals(id)) {
+				return archiveCar.get(i);
+			}
+			
+		}
+		return null;
+	}
+
+	@Override
+	public CarModel deleteCar(String id) {
+		for (int i=0; i<archiveCar.size(); i++) {
+			if(archiveCar.get(i).getId().equals(id)) {
+				return archiveCar.remove(i);
+			}
+		}
+		return null;
+	}
+}
---
+++ Tutorial-3/src/main/java/com/apap/tutorial3/service/CarService.java
@@ -0,0 +1,22 @@
+package com.apap.tutorial3.service;
+
+import java.util.List;
+
+import com.apap.tutorial3.model.CarModel;
+
+/**
+ * Car Service
+ * @author zhelialifa
+ *
+ */
+
+public interface CarService {
+	
+	void addCar(CarModel car);
+	
+	List<CarModel> getCarList();
+	
+	CarModel getCarDetail(String id);
+	
+	CarModel deleteCar(String id);
+}
